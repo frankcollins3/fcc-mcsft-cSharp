4fb8ebead4678469c8f05f69dc4cf4e45424466c
Frank Collins <fwc3rd@gmail.com>
2023-09-05 05:02:52 -0400

    Challenge 1 correct [5:02am]

--- string.cs
+++ string.cs
@@ -21,3 +21,14 @@ Console.WriteLine($"{versionText} {version}");
 string projectName = "Project-1";
 Console.WriteLine($@"C:\Output\{projectName}\Data");
 
+#### CHALLENGE 1:
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+// english output (no intermediate variables) --->
+Console.WriteLine("View English Output");
+Console.WriteLine($@"c:\Exercise\{projectName}\data.txt");
+
+// russian output: 
+Console.WriteLine($"\n{russianMessage}");
+Console.WriteLine($@"c:\Exercise\{projectName}\ru-RU\data.txt");

a85e868bb7763b92ecb46fbdae31644f7b849ad5
Frank Collins <fwc3rd@gmail.com>
2023-09-06 08:07:31 -0400

    subscription messages showing [8:07am]

---
+++ subscription.cs
@@ -0,0 +1,43 @@
+using System;
+
+class Program {
+
+    static Random random = new Random();
+    
+    // random.Next(12) 1 parameter means it's constrained by picking random number to be less than 12 So [0-11]
+    static int daysUntilExpired = random.Next(12); 
+    static int discountPercentage = 0;
+
+    static string lessThanTenMsg = "Your subscription will expire soon! Renew Now!";
+    static string lessThanFiveMsg = $"Your subscription expires in {daysUntilExpired} days Renew now and save 10%!";
+    static string oneMoreDayMsg = "Your subscription expires within a Day! Renew now and save 20%!";
+
+    static string expiredMsg = "Your subscription has expired!";
+
+    // string lessThanFiveMsg = "Your subscription expires in  days" Renew now and save 10%!"
+
+    static string checkExpired() 
+    {
+        if (daysUntilExpired <= 10) 
+        {
+            return lessThanTenMsg;
+        } else if (daysUntilExpired > 1 && daysUntilExpired <= 5) {            
+            return lessThanFiveMsg;
+        } else if (daysUntilExpired == 1) {
+            return oneMoreDayMsg;
+        } else if (daysUntilExpired == 0) {
+            return expiredMsg;
+        } else {
+          return "else";        
+    }
+
+    static void Main() 
+    {
+        string msg = checkExpired();
+        Console.WriteLine(discountPercentage);
+        Console.WriteLine($"daysUntilExpired: {daysUntilExpired}");
+        Console.WriteLine($"Msg: {msg}");
+    }
+
+
+}

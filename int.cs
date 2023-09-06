2fc0b37b945521ef7c2cf5b2e2b6734ac763c9a0
Frank Collins <fwc3rd@gmail.com>
2023-09-06 06:42:18 -0400

    largerNumber challenge [6:42am]
    string interpolation for Math.Max() works

---
+++ int.cs
@@ -0,0 +1,34 @@
+❤️🥋🎳🔒💡🥧👍👎⛳️🏳💥🔥🚨
+
+*** Larger Number ***
+
+using System; 
+
+# class Program {                     
+public static int getLarger(int num1, int num2) {
+    if (num1 > num2) 
+    {
+        return num1;
+    } else 
+    {
+        return num2;
+    }
+}
+
+
+static void Main(string[] args) 
+{
+    int biggerNum = getLarger(7, 11);
+    Console.WriteLine($"hey... {biggerNum} is the bigger Number.");
+
+    Console.WriteLine($" can also do this: \t {💥Math.Max(3, 99)💥} \t to find the bigger number.");
+}
+
+}
+
+[notes]
+# Class Program wraps all the code. I thought it only wrapped the public static int getLarger() function
+
+[notes]
+
+*** Larger Number ***

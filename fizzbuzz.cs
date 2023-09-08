181002c2b6f262dbd03d6dfcd3ebedc22ff095fd
Frank Collins <fwc3rd@gmail.com>
2023-09-08 00:06:42 -0400

    fizzbuzz [12:06am]

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -23,6 +23,18 @@ class Program {
 
 }
 
+// for (int i = 1; i < 101; i++)
+// {
+//     if ((i % 3 == 0) && (i % 5 == 0))
+//         Console.WriteLine($"{i} - FizzBuzz");
+//     else if (i % 3 == 0)
+//         Console.WriteLine($"{i} - Fizz");
+//     else if (i % 5 == 0)
+//         Console.WriteLine($"{i} - Buzz");
+//     else
+//         Console.WriteLine($"{i}");
+// }
+
 // before break statement:
 // Fizz: 	 0Buzz: 	 0FizzBuzz: 	 0
 

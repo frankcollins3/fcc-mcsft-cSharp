a4bf0c8dc76a843133fe45879df0be0d5846654b
Frank Collins <fwc3rd@gmail.com>
2023-09-09 22:23:29 -0400

    microsoft's example [10:23pm]

--- fourLetterOrderError.cs
+++ fourLetterOrderError.cs
@@ -15,3 +15,26 @@
           
         }
     }
+
+
+/*
+my example is closer to this one than the last algo even though both solved w/ success:
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(',');
+Array.Sort(items);
+
+foreach (var item in items)
+{
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
+    }
+}
+
+
+*/

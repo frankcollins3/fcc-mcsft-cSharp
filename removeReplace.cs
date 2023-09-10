803bbffad4c884206c2c5db46ca98cb46e9cf5a1
Frank Collins <fwc3rd@gmail.com>
2023-09-10 16:53:15 -0400

    fix issue # 29 [4:53pm]

---
+++ removeReplace.cs
@@ -0,0 +1,22 @@
+    using System;
+
+    class Program {
+
+        static void Main()
+        {
+
+        // string data = "12345John Smith          5000  3  ";
+        // 123455000  3 (starts at [5] = "J" && deletes 20 characters starting at "J" which .Removes() 20 characters, which is John Smith and most of the white space.)
+        
+        // string updatedData = data.Remove(5, 20);
+        // Console.WriteLine(updatedData);
+
+        // string data = "goodguy";
+        // string updatedData = data.Remove(1, 3);
+        // Console.WriteLine(updatedData);    
+        // gguy (removes [1] = "o" && 3 total digits "ood" returning string characters: "gguy");
+
+        
+        
+        }
+    }

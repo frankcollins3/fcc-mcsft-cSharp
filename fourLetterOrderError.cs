5dd7ef98e0c9bdb173e41ca2fc608ce3db641a7e
Frank Collins <fwc3rd@gmail.com>
2023-09-09 22:21:51 -0400

    almost posted issue [10:21pm]
    but solved. harder than it looks.

---
+++ fourLetterOrderError.cs
@@ -0,0 +1,17 @@
+    using System;
+
+    class Program {
+
+        static void Main()
+        {
+
+        foreach(string str in splitOrders) 
+        {
+            Console.Write($"{str} \n");
+            if (str.Length != 4) {
+                Console.Write($"{str} - error \n");
+            }
+        }
+          
+        }
+    }

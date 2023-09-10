4abdfb8fc010afa2f19c1e2a98ef2abd672ca349
Frank Collins <fwc3rd@gmail.com>
2023-09-10 17:21:50 -0400

    fix issue #30 [5:21pm]
    find HTML in string with .IndexOf() 
    delete HTML in string with .Remove()

--- removeReplace.cs
+++ removeReplace.cs
@@ -16,7 +16,33 @@
         // Console.WriteLine(updatedData);    
         // gguy (removes [1] = "o" && 3 total digits "ood" returning string characters: "gguy");
 
+        // string message = "This--is--ex-amp-le--da-ta";
+        // message = message.Replace("--", " ");
+        // message = message.Replace("-", "");
+        // Console.WriteLine(message);
+
+        // string message2 = "This is crazy!!!!****!!!*";
+        // message2 = message2.Replace("*", "!");           <!-- this takes the * asterisk and turns them into exclamation points! -->
+        // message2 = message2.Replace("!", " ");             // this does not replace every asterisk and every exclamation mark -->
+        // message2 = message2.Replace("*", " ");             
+        // Console.WriteLine(message2);
+
+        string quantity = "";
+        const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
         
+        int openingDivLessThan = input.IndexOf("<");
+        int openingDivGreaterThan = input.IndexOf(">");
+        Console.WriteLine(openingDivLessThan);
+        // quantity = input.Remove("<div>");
+        quantity = input.Remove(openingDivLessThan, openingDivGreaterThan + 1);
+        Console.WriteLine(input);
+        Console.WriteLine(quantity);
+
+
+
+
+
+
         
         }
     }

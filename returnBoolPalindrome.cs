d207d6dd6fcba03dce63ffc531000029212a15df
Frank Collins <fwc3rd@gmail.com>
2023-09-12 08:00:22 -0400

    [7:50am]

---
+++ returnBoolPalindrome.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {   
+
+    string[] words = {"racecar" ,"talented", "deified", "tent", "tenet"};
+
+    Console.WriteLine("Is it a palindrome?");
+    foreach (string word in words) 
+    {
+        Console.WriteLine($"{word}: {IsPalindrome(word)}");
+    }
+
+    bool IsPalindrome(string word) 
+    {
+        int start = 0;
+        int end = word.Length - 1;
+
+        while (start < end) 
+        {
+            if (word[start] != word[end]) 
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+
+    }
+}
